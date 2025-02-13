64f28c6e37d3b102bb765ef350e07f2d55a7820f
CSH2315 <choish00123@gmail.com>
2025-02-13 18:22:36 +0900

    feat: 국내 주식 뉴스 네이버 크롤링 추가

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from bs4 import BeautifulSoup
 
 app = FastAPI()
 
@@ -27,3 +28,33 @@ def get_global_stock_news(symbol: str):
         news_data = response.json()
         return news_data
     return {"error: Failed to fetch"}
+
+
+# 국내주식: 네이버 웹 크롤링
+@app.get("/news/korea/{symbol}")
+def get_korean_stock_news(symbol: str):
+    base_url = "https://search.naver.com/search.naver?where=news&query="
+    search_url = base_url + symbol
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(search_url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    news_list = []
+    articles = soup.select("div.news_area")
+
+    for article in articles[:20]:
+        title = article.select_one("a.news_tit").text
+        # pub_date = article.select_one(".info").text if article.select_one(".info") else "No Date information" # pubDate 가져오기
+        link = article.select_one("a.news_tit").get("href")
+        # guid = link # guid 가져오기
+        description = article.select_one(".news_dsc").text if article.select_one(".news_dsc") else "No description"
+
+        news_list.append({
+            "description": description,
+            # "guid": guid,
+            "link": link,
+            # "pubDate": pub_date,
+            "title": title
+        })
+
+    return {"symbol": symbol, "news": news_list}
